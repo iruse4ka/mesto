005f3a82442cc008e9cebb16b8f5da448f13910c
Irina Nistratova <iruse4ka@mail.ru>
2020-11-20 18:06:39 +0300

    Finished popup

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-let editButton = document.querySelector('.button__edit');
-let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
-width=430px,height=330px`;
+const edit = document.querySelector('.button_edit');
+const close = document.querySelector('.button_close');
+const popup = document.querySelector('.popup');
 
-editButton.addEventListener('click', addName);
+edit.addEventListener('click', openPopup);
+close.addEventListener('click', openPopup);
 
-function addName () {
-    window.open(url, name, params)
+function openPopup() {
+    popup.classList.toggle ('popup_opened');
 }
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__input');
+
+    function formSubmitHandler (evt) {
+        evt.preventDefault(); //
+
+        let nameInput = document.querySelector('.popup__input_type_name');
+            let jobInput = document.querySelector('.popup__input_type_role');
+
+        nameInput.getAttribute('value');
+        jobInput.getAttribute('value');
+
+        let profileName =document.querySelector('.profile__name');
+        let profileRole =document.querySelector('.profile__role');
+
+        profileName.textContent = nameInput.value;
+        profileRole.textContent = jobInput.value;
+
+    }
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+    let saveButton = document.querySelector('.button_save');
+
+saveButton.addEventListener('click', formSubmitHandler);
+saveButton.addEventListener('click', openPopup);
